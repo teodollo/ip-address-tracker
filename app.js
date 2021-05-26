de4985d522119c0efa5e33a0be3356688e315a2e
Teodoro Monteiro <58260478+teodollo@users.noreply.github.com>
2021-05-26 18:51:12 +0200

    app.js got renamed to map.js

--- app.js
+++
@@ -1,9 +0,0 @@
-
-var map = L.map('map').setView([53, -7], 52);
-L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=avotrdWWFNvP4VidXL8k',{
-tileSize: 512,
-zoomOffset: -1,
-minZoom: 1,
-attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e",
-crossOrigin: true
-}).addTo(map);
