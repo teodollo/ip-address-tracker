6e7ad4386920da466cdb3739c5b0f4f67406c165
Teodoro Monteiro <58260478+teodollo@users.noreply.github.com>
2021-05-26 18:57:27 +0200

    added map functionality
    need to fix map search, right now I can only to a ip location search one time. Have to figure out how I can clear previous map between searches

---
+++ map.js
@@ -0,0 +1,58 @@
+const input = document.getElementById('input');
+const searchField = document.getElementById('submit')
+const ipInfo = document.getElementById('ip-info')
+const ip = document.getElementById('ip')
+const regionAndCity = document.getElementById('location')
+const timeZone = document.getElementById('time-zone')
+const isp = document.getElementById('isp')
+
+function runMap() {
+    const Http = new XMLHttpRequest();
+    let IP = input.value;
+    let url="https://geo.ipify.org/api/v1?apiKey=at_POcwIp0yQ8aj7ZeaSw9hzFqiCd5FI&ipAddress=";
+    url += IP;
+    Http.open('GET', url);
+    Http.send();
+
+    Http.onreadystatechange = (e) => {
+        if(e.currentTarget.readyState == 4 && e.currentTarget.status == 200) {
+            const object = JSON.parse(Http.responseText);
+            const objectIp= object.ip;
+            console.log(object)
+            console.log(object.ip)
+            
+
+            
+            var mapid = L.map('map').setView([object.location.lat, object.location.lng], 14);
+            L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=avotrdWWFNvP4VidXL8k',{
+            tileSize: 512,
+            zoomOffset: -1,
+            minZoom: 1,
+            crossOrigin: true
+            }).addTo(mapid);
+        
+            L.marker([object.location.lat, object.location.lng]).addTo(mapid)
+                .openPopup();
+
+
+            ip.textContent = `${object.ip}`;
+            regionAndCity.textContent = `${object.location.region}, ${object.location.city} ${object.location.postalCode}`;
+            timeZone.textContent = `UTC ${object.location.timezone}`;
+            isp.textContent = `${object.isp}`;
+            ipInfo.style.display = 'block';
+
+        }
+
+
+    }     
+}
+
+
+searchField.addEventListener('click', e => {
+    e.preventDefault()
+    let map = document.getElementById('map');
+    map = null;
+    runMap();
+    input.value = '';
+
+})
