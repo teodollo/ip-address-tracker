aa39691df5454d4ea808eb6f3cf2e097f5b7e424
Teodoro Monteiro <58260478+teodollo@users.noreply.github.com>
2021-05-27 14:37:05 +0200

    replaced XMLHTTP request for fetch api
    switching out XMLHTTPRequest made the code shorter and upped the readability

--- map.js
+++ map.js
@@ -12,7 +12,7 @@ const isp = document.getElementById('isp')
 
 searchField.addEventListener('click', e => {
     e.preventDefault()//preventing window from reloading when submitting 
-    runMap();
+    runMap();//function that gives text input to api url that then makes the map and ip info, then runs map
     input.value = '';//clears input when event listener gets activated
 
 })
@@ -20,36 +20,26 @@ searchField.addEventListener('click', e => {
 //---------------------- FUNCTIONS ------------------------------------//
 
 function runMap() {
-    //Requesting Ip address information based on Ip Address added to geo.ipify url
-    const Http = new XMLHttpRequest();
-    let IP = input.value;
-    let url="https://geo.ipify.org/api/v1?apiKey=at_POcwIp0yQ8aj7ZeaSw9hzFqiCd5FI&ipAddress=";
-    url += IP;
-    Http.open('GET', url);
-    Http.send();
+    let IP = input.value
+    let url = "https://geo.ipify.org/api/v1?apiKey=at_POcwIp0yQ8aj7ZeaSw9hzFqiCd5FI&ipAddress=";
+    url += IP
 
-    //When Http request is ready map is created in docuent with latitude and longitude from geoLocation
-    Http.onreadystatechange = (e) => {
-        
-        if(e.currentTarget.readyState == 4 && e.currentTarget.status == 200) {
-            const ipInformation = JSON.parse(Http.responseText);//parsing the JSON string to an object
-
+    fetch(url)//fetch API url
+    .then(data=>{return data.json()}) //convert json string to readable object
+    .then(JSONToObject=>{return JSONToObject}) //return the object that becomes ipInformation
+    .then(ipInformation=>{
+        if (ipInformation.ip) { //if the object contains an ip run the functions
             //makes sure the div containing the map always gets cleared when searching for new ip
             document.getElementById('map-container').innerHTML = "<div id='map' style='width: 100%; height: 100%;'></div>";
 
             //Function that creates a map and locates it based on ipInformation
-            createMap(ipInformation);
+            createMap(ipInformation)
 
-            
             //Ip info container changes based on ipInformation object
-            ipInfoChange(ipInformation);
-        } 
-        else if (e.target.readyState == 4 && e.currentTarget.status != 200) { //If the Http request changes alert will be given
-            alert('Provide a valid IP Address.' + '\n' + 'IP format: 8.8.8.8')
-        }
-    }     
+            ipInfoChange(ipInformation)
+        } else ( alert('Provide a valid IP Address.' + '\n' + 'IP format: 8.8.8.8') ) //if object does not contain ip alert user
+    })
 }
-
 
 //Create map on page with location that corelates with ip location and a marker 
 const createMap = (ipInformation) => {
